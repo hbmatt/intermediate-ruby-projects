ec7f58ed38b600bcc279d9e4ff13ea69505d7144
hbmatt <hannahbmatt@gmail.com>
2020-07-29 14:21:59 -0500

    Added README.md and fibonacci.rb

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a collection of intermediate Ruby projects from The Odin Project.
+
+## Fibonacci
+Created two methods, one using iteration and the other using recursion, which take a number and return that many members of the Fibonacci sequence.
---
+++ fibonacci.rb
@@ -0,0 +1,21 @@
+def fibs(n, result = [])
+  i = 1
+  until i > n
+    if i == 1
+      result.push(0)
+    elsif i == 2
+      result.push(1)
+    else
+      result.push(result[i - 2] + result[i - 3])
+    end
+    i += 1
+  end
+  result
+end
+
+
+def fibs_rec(n)
+  return [0] if n == 1
+  return [0, 1] if n == 2
+  fibs_rec(n - 1).push(fibs_rec(n - 1)[-1] + fibs_rec(n - 1)[-2])
+end
